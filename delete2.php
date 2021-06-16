0b28e4b3d46249d4d88bbe78bdd7741212ecb6a0
scottnguyen0412 <74314165+scottnguyen0412@users.noreply.github.com>
2021-06-16 20:00:05 +0700

    Update delete2.php

--- delete2.php
+++ delete2.php
@@ -1,8 +1,8 @@
 <?php
-$host_heroku = "ec2-54-224-194-214.compute-1.amazonaws.com";
-$db_heroku = "d1b7erismhj8n3";
-$user_heroku = "irihpzeafcijwa";
-$pw_heroku = "f95d0c3c2fbeff2f8f8a836c4fbc1a31a6018cf2adbfb635f1829a96e436b107";
+   $host_heroku = "ec2-34-233-114-40.compute-1.amazonaws.com";
+			$db_heroku = "d63vsqeg1u1bhr";
+			$user_heroku = "vcljjiechthrbs";
+			$pw_heroku = "91e24a4f6c609d6e3bd3c33019a26933d378b76f8e8c58956c617060bdf2bc12";
 $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
 $pg_heroku = pg_connect($conn_string);
 if (!$pg_heroku)
